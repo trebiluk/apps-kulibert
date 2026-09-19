0f51b5a9c1fe4eec81800bb79815ebc29b4b5784
Trebzy <rkulibert@gmail.com>
2026-09-18 20:18:28 -0400

    HouseKit styling pass v3

--- housekit/sw.js
+++ housekit/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "housekit-v2";
+const CACHE = "housekit-v3";
 const FILES = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
